a3a6fb3d1851a7cfdfa18dc0b1dbbebe4efc9b37
Lucas GILHARD <l.gilhard@gmail.com>
2020-02-21 23:01:10 +0100

    define theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,15 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF090B22),
+        accentColor: Color(0xFFEA1456),
+        scaffoldBackgroundColor: Color(0xFF090B22),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Color(0xFFEA1456),
+          foregroundColor: Colors.white,
+        ),
+      ),
       home: InputPage(),
     );
   }
