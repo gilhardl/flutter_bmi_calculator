6cb9fc0866b727743db8ac8e6cfa145f755fa23d
Lucas GILHARD <l.gilhard@gmail.com>
2020-03-04 11:19:04 +0100

    refactor input_page to use reusable widget and constants

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const footerHeight = 80.0;
+const footerColor = Color(0xFFEA1556);
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF101427);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -9,75 +14,79 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('BMI CALCULATOR'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                    ),
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: BmiCard(
+                    color: activeCardColor,
                   ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                    ),
+                ),
+                Expanded(
+                  child: BmiCard(
+                    color: activeCardColor,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                    ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: BmiCard(
+                    color: activeCardColor,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                    ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: BmiCard(
+                    color: activeCardColor,
                   ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                    ),
+                ),
+                Expanded(
+                  child: BmiCard(
+                    color: activeCardColor,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ));
+          ),
+          Container(
+            color: footerColor,
+            margin: EdgeInsets.only(top: 12.0),
+            width: double.infinity,
+            height: footerHeight,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BmiCard extends StatelessWidget {
+  BmiCard({@required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      ),
+    );
   }
 }
