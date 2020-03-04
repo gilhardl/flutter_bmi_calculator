9abb9e19387b07a2f289a987c089112ad9afad65
Lucas GILHARD <l.gilhard@gmail.com>
2020-03-05 00:23:57 +0100

    Update input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -72,6 +72,7 @@ class _InputPageState extends State<InputPage> {
             child: BmiCard(
               color: kActiveCardColor,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text(
                     'HEIGHT',
